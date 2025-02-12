0e78cac34b9d86322b3cade1bd17d222a9688179
Ozbolt Menegatti <ozbolt.menehatti@gmail.com>
2025-02-12 14:15:13 +0100

    use mozjpeg and jpegoptim to reduce the size of the images some more

--- scripts/convert_imgs.sh
+++ scripts/convert_imgs.sh
@@ -3,11 +3,17 @@
 orig="original/"
 out="new/"
 ctr=0
+cjpeg="../../gits/mozjpeg/build/cjpeg"
 
-for img in $(find $orig)
+for img in $(find $orig -type f)
 do
   echo $img
-  convert -type Grayscale -resize x800 -quality 80 $img "$out$ctr.jpg"
+  convert -type Grayscale -resize x800 -quality 100 $img "$out$ctr.notoptim.jpg"
+  $cjpeg -optimize -quality 80 "$out$ctr.notoptim.jpg" >"$out$ctr.jpg"
+  jpegoptim -s "$out$ctr.jpg"
+
+  rm "$out$ctr.notoptim.jpg"
+
   ctr=$(($ctr+1))
 done
 
